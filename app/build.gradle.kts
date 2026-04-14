2036bfb741b72aa4a91b6f104c3d3170477b3165
jiaweiyaya <jiaweiya18969004179@outlook.com>
2026-04-14 15:36:07 +0800

    修改版本号为V2.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.jiaweiya.flowcourse"
         minSdk = 26
         targetSdk = 36
-        versionCode = 22
-        versionName = "2.3.1"
+        versionCode = 24
+        versionName = "2.3.3"
 
         ndk {
             abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a", "x86_64"))
